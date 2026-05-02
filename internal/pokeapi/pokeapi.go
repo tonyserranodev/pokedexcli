5edf2dd6c34bde936b63850199fa6bbcbbf7eebb
Tony Serrano <tonyserranodev@proton.me>
2026-05-02 18:04:18 -0500

    feat: add cache for locations retrieved from GetLocations

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,11 +1,18 @@
-// Package pokeapi provides functions for interacting with the Pokemon API at https://pokeapi.co
+// Package pokeapi provides functions for interacting with the Pokemon API at https://pokeapi.co.
 package pokeapi
 
 import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/tonyserranodev/pokedexcli/internal/pokecache"
 )
+
+const cacheInterval = 5 * time.Second
+
+var cache = pokecache.NewCache(cacheInterval)
 
 type LocationsResponse struct {
 	Count    int     `json:"count"`
@@ -18,9 +25,18 @@ type LocationsResponse struct {
 }
 
 func GetLocations(pageURL *string) (LocationsResponse, error) {
+
 	url := "https://pokeapi.co/api/v2/location-area/"
 	if pageURL != nil {
 		url = *pageURL
+	}
+
+	if val, ok := cache.Get(url); ok {
+		var locations LocationsResponse
+		if err := json.Unmarshal(val, &locations); err != nil {
+			return LocationsResponse{}, err
+		}
+		return locations, nil
 	}
 
 	res, err := http.Get(url)
@@ -33,6 +49,8 @@ func GetLocations(pageURL *string) (LocationsResponse, error) {
 	if err != nil {
 		return LocationsResponse{}, err
 	}
+
+	cache.Add(url, body)
 
 	var locations LocationsResponse
 	if err := json.Unmarshal(body, &locations); err != nil {
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,57 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries map[string]cacheEntry
+	mu      sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		entries: map[string]cacheEntry{},
+	}
+	go c.reapLoop(interval)
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if entry, ok := c.entries[key]; ok {
+		return entry.val, true
+	}
+	return nil, false
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.mu.Lock()
+		for k, entry := range c.entries {
+			if time.Since(entry.createdAt) > interval {
+				delete(c.entries, k)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
